5254221d7047f13f4b3d7e9bc70f810875f2ae79
CaribouW <1071956678@qq.com>
2020-02-15 21:23:29 +0800

    简单搭建mq

---
+++ oasis-paper/src/main/java/com/example/oasispaper/OasisPaperApplication.java
@@ -0,0 +1,13 @@
+package com.example.oasispaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OasisPaperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OasisPaperApplication.class, args);
+	}
+
+}
---
+++ oasis-paper/src/test/java/com/example/oasispaper/OasisPaperApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.oasispaper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OasisPaperApplicationTests {
+    @Test
+    public void run(){
+    }
+}
