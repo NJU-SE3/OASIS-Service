d92314f7c8a026f0fcf59ecab0592101291f20d4
CaribouW <1071956678@qq.com>
2020-02-19 09:32:42 +0800

    添加config

---
+++ oasis-config-server/src/main/java/com/example/oasisconfigserver/OasisConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.example.oasisconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class OasisConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OasisConfigServerApplication.class, args);
+    }
+
+}
---
+++ oasis-config-server/src/test/java/com/example/oasisconfigserver/OasisConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oasisconfigserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OasisConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
