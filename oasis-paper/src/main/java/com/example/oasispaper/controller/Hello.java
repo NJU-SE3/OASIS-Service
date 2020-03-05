289ec3c518152c26b70ea8e7b46781a2008e8efa
CaribouW <1071956678@qq.com>
2020-03-05 21:03:07 +0800

    update:更新pom

--- oasis-paper/src/main/java/com/example/oasispaper/controller/Hello.java
+++ oasis-paper/src/main/java/com/example/oasispaper/controller/Hello.java
@@ -1,6 +1,7 @@
 package com.example.oasispaper.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,15 @@ public class Hello {
     @GetMapping("/")
     public String hello() {
         return "hello world";
+    }
+
+    @GetMapping("/{name}")
+    public String h(@PathVariable String name) {
+        return name;
+    }
+
+    @GetMapping("/hek")
+    public String h1() {
+        return "name";
     }
 }
