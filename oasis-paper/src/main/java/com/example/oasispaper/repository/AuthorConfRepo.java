bb65990e028f9d80a941f8b34f5bbc3454e5bea3
CaribouW <1071956678@qq.com>
2020-03-08 18:01:01 +0800

    remove paper module

--- oasis-paper/src/main/java/com/example/oasispaper/repository/AuthorConfRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.oasispaper.repository;
-
-import com.example.oasispaper.model.Publication;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorConfRepo extends JpaRepository<Publication, Integer> {
-}
--- oasis-paper/src/main/java/com/example/oasispaper/repository/PaperRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.oasispaper.repository;
-
-import com.example.oasispaper.model.Paper;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaperRepository extends JpaRepository<Paper, Integer> {
-}
