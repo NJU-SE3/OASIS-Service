6356af5cfa00298cb23947bd5ff9d9d375fb7421
CaribouW <1071956678@qq.com>
2020-04-12 14:01:29 +0800

    update: move eureka to nacos

--- oasis-document/src/main/java/com/example/oasisdocument/OasisDocumentApplication.java
+++ oasis-document/src/main/java/com/example/oasisdocument/OasisDocumentApplication.java
@@ -2,16 +2,15 @@ package com.example.oasisdocument;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableTransactionManagement
 @EnableAsync
 @EnableCaching
